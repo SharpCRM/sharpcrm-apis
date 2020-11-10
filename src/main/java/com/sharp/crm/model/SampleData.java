801a0ef766f6d0dac82185990bb088446839d403
K, Sumateja <sumateja_k@optum.com>
2020-11-10 17:44:50 +0530

    Updated model

--- src/main/java/com/sharp/crm/model/SampleData.java
+++ src/main/java/com/sharp/crm/model/SampleData.java
@@ -1,9 +1,75 @@
 package com.sharp.crm.model;
 
+import java.util.Date;
+
 public class SampleData {
+    
+    private String dataID;
     
     private String fileName;
     
     private String fileAttachment;
+    
+    private String dataType;
+    
+    private String 
+
+    private Date lastUpdatedTimeStamp;
+    
+    private Date createdTimeStamp;
+    
+    public String getDataType() {
+        return dataType;
+    }
+
+    public void setDataType(String dataType) {
+        this.dataType = dataType;
+    }
+
+   
+    
+  
+
+    public String getDataID() {
+        return dataID;
+    }
+
+    public void setDataID(String dataID) {
+        this.dataID = dataID;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileAttachment() {
+        return fileAttachment;
+    }
+
+    public void setFileAttachment(String fileAttachment) {
+        this.fileAttachment = fileAttachment;
+    }
+
+    public Date getLastUpdatedTimeStamp() {
+        return lastUpdatedTimeStamp;
+    }
+
+    public void setLastUpdatedTimeStamp(Date lastUpdatedTimeStamp) {
+        this.lastUpdatedTimeStamp = lastUpdatedTimeStamp;
+    }
+
+    public Date getCreatedTimeStamp() {
+        return createdTimeStamp;
+    }
+
+    public void setCreatedTimeStamp(Date createdTimeStamp) {
+        this.createdTimeStamp = createdTimeStamp;
+    }
+    
+    
     
 }
